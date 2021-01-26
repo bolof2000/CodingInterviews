da6089befb3c14d5660eb8102035649f3845fd43
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-01-26 18:55:26 +0000

    01/26 solutions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution(object):
     def twoNumberSum(self,array, targetSum):
         array.sort()
@@ -14,5 +15,51 @@ class Solution(object):
 
         return []
 
+    def randomNote(self,magazine,word):
+      dic = defaultdict(int)
+      for char in magazine:
+        dic[char] +=1
+      
+      for c in word:
+        if dic[c] <=0:
+          return False
+        dic[c] -=1
+      return True
+
+
+    def randomNote2(self,magazine,word):
+      check = all(item in word for item in magazine)
+
+      return check
+
+
+    def duplicateNumber(self,nums):
+
+      occurrence = {}
+      for num in nums:
+        occurrence[num] = occurrence.get(num,0)+1
+        for key,value in occurrence.items():
+          if value ==1:
+            return key   
+
+    def duplicateNumber2(self,nums):
+      unique =0
+      for num in nums:
+        unique ^=num 
+
+      return unique
+
+         
+
+
+
 test = Solution()
-print(test.twoNumberSum([1,2,3,4,5],9))
+magazing = ['A','B','C','D','E','F','D','F']
+#dic = {A:1,B:1,C:1,D:2,F:2}
+word = "DFDDF"
+#print(sorted(list(word)))
+#print(sorted(magazing))
+print(test.randomNote(magazing,word))
+print(test.randomNote2(magazing,word))
+#print(test.twoNumberSum([1,2,3,4,5],9))
+print(test.duplicateNumber2([1,2,3,4,5,1,2,3,4]))
